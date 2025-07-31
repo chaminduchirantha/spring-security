8af730a5a3c61c33259a261ac00e6c8563e633f2
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-07-31 11:28:07 +0530

    create the entity

---
+++ src/main/java/lk/ijse/gdse/springsecurity/entity/Role.java
@@ -0,0 +1,6 @@
+package lk.ijse.gdse.springsecurity.entity;
+
+public enum Role{
+    ADMIN, USER
+
+}
---
+++ src/main/java/lk/ijse/gdse/springsecurity/entity/User.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse.springsecurity.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
