9747ae331abdd9506b83253040bc26fd5d2a87e6
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-07-31 14:45:17 +0530

    create AuthService class

---
+++ src/main/java/lk/ijse/gdse/springsecurity/service/AuthService.java
@@ -0,0 +1,50 @@
+package lk.ijse.gdse.springsecurity.service;
+
+import lk.ijse.gdse.springsecurity.Dto.AuthDto;
+import lk.ijse.gdse.springsecurity.Dto.AuthResponseDto;
+import lk.ijse.gdse.springsecurity.Dto.RegisterDto;
+import lk.ijse.gdse.springsecurity.entity.Role;
+import lk.ijse.gdse.springsecurity.entity.User;
+import lk.ijse.gdse.springsecurity.repo.UserRepo;
+import lk.ijse.gdse.springsecurity.utill.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    public AuthResponseDto authenticate(AuthDto authDto) {
+        User user = userRepo.fidByUserName(authDto.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        if (passwordEncoder.matches(authDto.getPassword(),user.getUsername())){
+            throw new BadCredentialsException("Invalid Password");
+        }
+
+        String token = jwtUtil.generateToken(authDto.getUsername());
+        return new AuthResponseDto(token);
+    }
+
+
+    public String register(RegisterDto registerDTO) {
+        if (userRepo.fidByUserName(registerDTO.getUsername()).isPresent()) {
+            throw new RuntimeException("Username is already exist");
+        }
+        User user = User.builder()
+                .username(registerDTO.getUsername())
+                .password(passwordEncoder.encode(registerDTO.getPassword()))
+                .role(Role.valueOf(registerDTO.getRole()))
+                .build();
+        userRepo.save(user);
+        return "User registered successfully";
+    }
+
+
+
+}
