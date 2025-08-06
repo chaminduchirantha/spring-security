44deeba946c7417ec6c244ffd4235331b800579c
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-08-06 22:32:07 +0530

    update auth controller

--- src/main/java/lk/ijse/gdse/springsecurity/controller/AuthController.java
+++ src/main/java/lk/ijse/gdse/springsecurity/controller/AuthController.java
@@ -2,18 +2,19 @@ package lk.ijse.gdse.springsecurity.controller;
 
 import lk.ijse.gdse.springsecurity.Dto.ApiResponce;
 import lk.ijse.gdse.springsecurity.Dto.AuthDto;
+import lk.ijse.gdse.springsecurity.Dto.AuthResponseDto;
 import lk.ijse.gdse.springsecurity.Dto.RegisterDto;
 import lk.ijse.gdse.springsecurity.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthController {
     private final AuthService authService;
 
@@ -31,7 +32,17 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponce> login(@RequestBody AuthDto authDTO) {
-        return ResponseEntity.ok(new ApiResponce(200,
-                "OK", authService.authenticate(authDTO)));
+        try {
+            AuthResponseDto auth = authService.authenticate(authDTO);
+            return ResponseEntity.ok(new ApiResponce(200, "OK", auth));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(new ApiResponce(401, "Invalid username or password", null));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponce(500, "Server error", null));
+        }
     }
 }
